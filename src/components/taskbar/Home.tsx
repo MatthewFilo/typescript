1d817595b1e01dbb460a0c1c36ab4401fabd3fd2
Matthew Filo <mattfilo83@gmail.com>
2025-04-15 21:36:59 -0500

    Added updates and website should be ready for rough-draft hosting

--- src/components/taskbar/Home.tsx
+++ src/components/taskbar/Home.tsx
@@ -4,7 +4,7 @@ import Logo from '../../images/home_page/matthew_filo_face.png'
 function Home() {
   return (
     <div>
-      <img src={Logo} alt="Matthew Filo" className="home-logo" />
+      <img src={Logo} alt="Matthew Filo" className="home-logo" style={{width: '75%', margin: '0', padding: '0'}}/>
     </div>
   );
 }
